e99c23bb3af383c30a35ef5388f6a57b01025e18
dinka09 <dinkawolde09@gmail.com>
2022-09-03 07:08:50 -0700

    omitted ambiguit variable

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        l = 1
+        n = 1
         for j in i:
-            if l == len(i):
+            if n == len(i):
                 print("{:d}".format(j), end="")
             else:
                 print("{:d}".format(j), end=" ")
-            l = l + 1
+            n = l + 1
         print()
