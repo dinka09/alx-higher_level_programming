861ec6cc1bb736d9a906ea0cc1c901b53085326d
dinka09 <dinkawolde09@gmail.com>
2022-09-05 06:49:01 -0700

    first commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for character in str + "\n":
         if (ord(character) >= 97 and ord(character) < 123):
-            print("{}".format(chr(ord(character) - 32)), end+"")
+            print("{}".format(chr(ord(character) - 32)), end="")
         else:
             print("{}".format(character), end="")
