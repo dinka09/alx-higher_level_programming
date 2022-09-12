a9d0065e3b23407a6dcb77f0716a29a2622580d5
dinka09 <dinkawolde09@gmail.com>
2022-09-12 08:44:28 -0700

    fixed pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             value += 1
-        except:
+        except Exception:
             continue
     print()
     return value
