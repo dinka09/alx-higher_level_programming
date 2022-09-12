7b55dbaaf8b5cc89b4012c42fa77106289230170
dinka09 <dinkawolde09@gmail.com>
2022-09-12 08:25:51 -0700

    correction on endex error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
-    print_value = 0
+    value = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            print_value += 1
+            value += 1
         except:
             continue
-        print()
-        return print_value
+    print()
+    return value
