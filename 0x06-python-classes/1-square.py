8280c6bf5f71d3f3aafddaad9f7bbf88bb49a5e6
dinka09 <dinkawolde09@gmail.com>
2022-09-14 12:34:44 -0700

    documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """
 Args:
-size: size of the object's are wich should be considered as the private and setted as private attributes
+size: size of the object's are wich should be
+considered as the private and setted as private attributes
 """
+
+
 class Square:
+    """
+    Square class to define square
+    """
     def __init__(self, size):
+        """
+        This methods will determine the size of the given objects
+        """
         self.__size = size
