f7285b9833250a1a30b7d19edd6e8158efd1b18e
dinka09 <dinkawolde09@gmail.com>
2022-09-14 13:15:04 -0700

    first commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    The square class to define the size of the object
+    """
+    def __init__(self, size=0):
+        """
+        size: attributes that define the size of the object with integer value
+        """
+        if type(size) != int:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        to get the value of the size of the object
+        """
+        ar = self.__size * self.__size
+        return ar
