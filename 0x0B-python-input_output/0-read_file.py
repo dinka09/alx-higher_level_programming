c2094a36d4ad479d3ff56321132d4e266da3da03
dinka09 <dinkawolde09@gmail.com>
2022-09-29 11:45:49 +0300

    added documents

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-"""The function reads the text file as well as prints to stdout"""
+    """The function reads the text file as well as prints to stdout"""
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
