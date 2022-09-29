563f368e3bdbfbd767d974f2431ede4cf64d6cd0
dinka09 <dinkawolde09@gmail.com>
2022-09-29 19:28:04 +0300

    first commit

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Prints the text"""
 
 
 import json
 
 
 def from_json_string(my_str):
+    """a function that returns an object (Python data structure)
+    represented by a JSON string"""
     return json.loads(my_str)
