408e1345da08a94ff51087b32c51abdb759a3a24
dinka09 <dinkawolde09@gmail.com>
2022-09-29 17:34:04 +0300

    first commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """The function reads the text file as well as prints to stdout"""
-    with open(filename, 'r', encoding = "UTF -8) as f:
-        return len(list(f))
+    with open(filename, "w", encoding="UTF-8") as f:
+        return f.write(text)
