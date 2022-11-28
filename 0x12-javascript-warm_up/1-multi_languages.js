9fe0d25c334e246694a0a97cd3617d254b3954a8
dinka09 <dinkawolde09@gmail.com>
2022-11-28 21:08:48 +0300

    first commut

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let TheFirstLine = "C is fun";
+let TheSecondLine = "Python is cool";
+let TheThirdLine = "JavaScript is amazing";
+
+console.log(TheFirstLine);
+console.log(TheSecondLine);
+console.log(TheThirdLine);
