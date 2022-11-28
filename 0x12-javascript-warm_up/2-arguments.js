88673f13f78964bfa277ff6ed7161a13cc292afc
dinka09 <dinkawolde09@gmail.com>
2022-11-28 21:36:05 +0300

    first commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Prints a message based on the number of arguments passed
 
-if (process.argv.length === 2) {
+if (process.argv.length === 0) {
     console.log('No argument');
-  } else if (process.argv.length === 3) {
+  } else if (process.argv.length === 1) {
     console.log('Argument found');
   } else {
     console.log('Arguments found');
