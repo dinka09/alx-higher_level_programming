9fa2a49e3e0a4da879df09e4942142449f3cc352
dinka09 <dinkawolde09@gmail.com>
2022-11-28 21:17:59 +0300

    first commit

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
+// print three lines
 
-let TheFirstLine = "C is fun";
-let TheSecondLine = "Python is cool";
-let TheThirdLine = "JavaScript is amazing";
-
-console.log(TheFirstLine);
-console.log(TheSecondLine);
-console.log(TheThirdLine);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
