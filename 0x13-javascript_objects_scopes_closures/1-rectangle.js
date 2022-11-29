dba0ea950a6e717433946fec0f79f1b8a1a91f87
dinka09 <dinkawolde09@gmail.com>
2022-11-29 20:49:49 +0300

    first commit

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Class Rectangle that defines rectangle
 
-module.exports = Rectangle {
-    constructor(w, h) {
+ class Rectangle {
+    constructor(w, h) { 
         this.width = w;
         this.heigth = h;
     };
