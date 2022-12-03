c24f0b67069d6e79fce732a7ffeeae88f9473961
dinka09 <dinkawolde09@gmail.com>
2022-12-03 08:36:14 +0300

    permsion

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // scripts that prints three line
 
-const stringArray = ['C is fun", "Python is cool", "JavaScript is amazing'];
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i <= stringArray.length-1; i++) {
   console.log(stringArray[i]);
 };
