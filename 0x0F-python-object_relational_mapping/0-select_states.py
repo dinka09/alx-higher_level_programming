8146e801f6740e340533bf1b54f597caace0b9e1
dinka09 <dinkawolde09@gmail.com>
2022-12-03 01:49:38 -0800

    first commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Write a script that lists all states from the database hbtn_0e_0_usa """
 
-from MySQLdb import _mysql
+import MySQLdb
+from sys import argv
+
 def query_table():
-    db=_mysql.connect(host="localhost",port=3306,user="mysql",password="mysql",database="hbtn_0e_0_usa")
+    db=MySQLdb.connect(host="localhost",user=argv[1],password=argv[2],port=3306,database=argv[3])
     print(db.query("select * from states group by id"))
 if __name__ == '__main__':
+    """Access to the database and get the state from it"""
     query_table()
