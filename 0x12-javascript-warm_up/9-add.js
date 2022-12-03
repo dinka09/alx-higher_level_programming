ddc2c2e02d59ffd6fc4cc23eebb622d82dae15ad
dinka09 <dinkawolde09@gmail.com>
2022-12-03 07:34:50 +0300

    first commit

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 // prints the addition of 2 integers
 
 function add (a, b) {
-    return parseInt(a) + parseInt(b);
-  }
+  return parseInt(a) + parseInt(b);
+}
   
-  console.log(add(process.argv[2], process.argv[3]));
+console.log(add(process.argv[2], process.argv[3]));
