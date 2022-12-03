9d89238d77adb95dc3111368c636fbadad402150
dinka09 <dinkawolde09@gmail.com>
2022-12-03 13:35:57 +0300

    first commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 from sys import argv
 
 def query_table():
-    db=MySQLdb.connect(host="localhost",port=3306,user=argv[1],password=argv[2],db="hbtn_0e_0_usa")
+    db=MySQLdb.connect(host="localhost",port=3306,user=argv[1],password=argv[2],database="hbtn_0e_0_usa")
     print(db.query("select * from states group by id"))
 if __name__ == '__main__':
     query_table()
