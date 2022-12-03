48253b50366ef2651aad9b07be4550bae915b68f
dinka09 <dinkawolde09@gmail.com>
2022-12-03 07:43:27 +0300

    first commit

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-// executes x times a function.
+// modifies the value of myVar to 333
 
-exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
-};
+myVar = 333;
+
