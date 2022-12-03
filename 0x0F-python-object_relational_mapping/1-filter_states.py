8fd7ff51262d2eeb1888a9d594d9ba61caa16bdc
dinka09 <dinkawolde09@gmail.com>
2022-12-03 20:18:15 +0300

    first commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+"""Script lists all states from database hbtn_0e_0_usa
+Takes three arguments:
+    mysql username
+    mysql password
+    database name
+Connects to default host (localhost) and port (3306)
+"""
+
+if __name__== "__main__":
+    """lists the value by sorting"""
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WhERE name like 'N%'\ORDER BY states.id ASC""")
+    names = c.fetchall()
+    for name in names:
+        print(row)
+    c.close()
+    db.close()
