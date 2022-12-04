9fe63184905d36aee3dc969441c33f0e56910faf
dinka09 <dinkawolde09@gmail.com>
2022-12-04 08:43:17 +0300

    first commit -m

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 // executes x times a function.
 
-<<<<<<< HEAD
 exports.addMeMaybe = function (x, theFunction) {
-=======
-exports.callMeMoby = function (x, theFunction) {
->>>>>>> 12800417dc0880e2516e08ee4fba62aa84791d91
   x++;
   theFunction(x);
 };
