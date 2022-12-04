5fb927e684d75deecfdcb13208af5878bddd4e5f
dinka09 <dinkawolde09@gmail.com>
2022-12-04 13:46:30 +0300

    first commit

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script lists all cities from database hbtn_0e_0_usa
+Takes three arguments:
+    mysql username
+    mysql password
+    database name
+Connects to default host (localhost) and port (3306)
+"""
+
+if __name__ == "__main__":
+    """"access to the data base and lists cities"""
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("""SELECT
+              cities.id, cities.name
+              FROM cities
+              JOIN states
+              ON cities.state_id = states.id
+              WHERE states.name LIKE BINARY %(state_name)s
+              ORDER BY cities.id ASC""", {'state_name': argv[4]})
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
