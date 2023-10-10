b80aee82dc04bff8e7bbfcc97142a902c66d7f52
dinka09 <dinkawolde09@gmail.com>
2023-10-10 20:25:31 +0300

    add comment wyh -1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    listlength = len(my_list) - 1
+    listlength = len(my_list) - 1 # -1 is used since the index starts to be counted from 0
     if (idx < 0 or idx > listlength):
         return (None)
     else:
